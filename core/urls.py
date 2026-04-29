2cacee50c25791c5b149baa5f3992d02f6e157d1
abebedereje170-boop <abebedereje170@gmail.com>
2026-04-29 10:54:15 +0000

    Add update/delete todo features

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
+    path("", views.get_todos, name="index"),
     path("todos/", views.get_todos, name="get_todos"),
     path("todos/<int:todo_id>/", views.get_todo_by_id, name="get_todo_by_id"),
     path("todos/create/", views.create_todo, name="create_todo"),
+    path("todos/<int:todo_id>/update/", views.update_todo, name="update_todo"),
+    path("todos/<int:todo_id>/delete/", views.delete_todo, name="delete_todo"),
 ]
